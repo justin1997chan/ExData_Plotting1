986510ec4b65b7628e8fbfdd87f6dca54c199930
justin1997chan <justin1997chan@gmail.com>
2020-06-01 23:11:08 +0800

    plot 4

--- plot4.R
+++ plot4.R
@@ -13,17 +13,17 @@ subconsum$datetime <- as.POSIXct(datetime)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(subconsum, {
   plot(Global_active_power~datetime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
+       ylab="Global Active Power", xlab="")
   plot(Voltage~datetime, type="l", 
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage", xlab="")
   plot(Sub_metering_1~datetime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
+       ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~datetime,col='Red')
   lines(Sub_metering_3~datetime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~datetime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
+       ylab="Global_reactive_power",xlab="")
 })
 
 dev.copy(png, file="plot4.png", height=480, width=480)
